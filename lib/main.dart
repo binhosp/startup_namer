b6e6143f228e783383d6f431b59dbcdd5ae54429
Fabio Santana <fpascoal@atech.com.br>
2018-05-23 00:02:31 -0300

    Step 6

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,9 @@ class MyApp extends StatelessWidget {
 
     return new MaterialApp(
 
-      title: 'Welcome to Flutter',
+        title: 'Welcome to Flutter',
 
-      home: new RandomWords()
+        home: new RandomWords()
 
     );
   }
@@ -36,6 +36,9 @@ class RandomWordsState extends State<RandomWords>{
     return new Scaffold (
       appBar: new AppBar(
         title: new Text('Startup Name Generator'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
@@ -87,6 +90,39 @@ class RandomWordsState extends State<RandomWords>{
       },
 
     );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          final tiles = _saved.map(
+                (pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile
+              .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+              .toList();
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Saved Suggestions'),
+            ),
+            body: new ListView(children: divided),
+          );
+        },
+
+      ),
+    );
+
   }
 }
 
